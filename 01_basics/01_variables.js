a05a1203516b33d59793975d0308dee4f838bb32
ashishp101 <150328781+ashishp101@users.noreply.github.com>
2023-11-23 18:20:07 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 141;
+let accountEmail = "plant@gmail.com";
+var accountPwd = "1234";
+accountCity = "Goa";
+let accountState;
+
+//accountId = 2;  // not allowed 
+accountEmail = "green@gmail.com";
+accountPwd = "1111";
+accountCity = "Delhi";
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPwd,accountCity,accountState]);
