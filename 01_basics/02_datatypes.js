2ca8cc76951d3dc69ce69c1cc984ea8598b3d265
ashishp101 <150328781+ashishp101@users.noreply.github.com>
2023-11-23 18:20:31 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";  // treat all js code as newer version
+
+// alert("Hello");  //we are using node.js ,not browser
+console.log(3+2); 
+
+let name="abc";
+let age="40";
+let decision=false;
+let a=null;
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => for use uniqueness 
+
+
+//object 
+
+console.log(typeof "abc");
+console.log(typeof decision);
+console.log(typeof null);
+console.log(typeof undefined);
