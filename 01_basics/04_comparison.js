2613362978bec5be513ddd9749986ea58d498d4a
ashishp101 <150328781+ashishp101@users.noreply.github.com>
2023-11-24 07:12:26 +0000

    comparison opeartions

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+console.log(2>1);
+console.log(10<5);
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);;
+console.log(null>=0);
+console.log(undefined>=0);
+
+// strict check 
+//it checks both things datatype and value
+
+console.log(3===3);
+console.log(4===5);
+console.log("4"===4);
+console.log("5"==="5");
