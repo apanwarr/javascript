36dd790944967643e1e1c17c982c5d1aeca8030b
ashishp101 <150328781+ashishp101@users.noreply.github.com>
2023-11-24 06:53:41 +0000

    conversion and operations

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -26,3 +26,26 @@ let no = 33;
 let no2 = String(no);
 console.log(no2);
 console.log(typeof no2);
+
+
+// *********************************operations********************************** //
+
+let value=3;
+let neg=-value;
+console.log(value);
+console.log(neg);    // opeartions   ........*,/,+,-,%
+
+console.log("1"+2);
+console.log(2+"4");
+console.log("3"+2 +1);
+console.log(1+2+"3");
+console.log((3+4)*5%3);
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3;
+
+num1=num2=num3=2+2;
+console.log(num1,num2,num3);
+
+console.log(++num1);
