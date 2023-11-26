a68f21aa3153404bc6005e8edd708ba71f48605a
Ashish Panwar <150328781+ashishp101@users.noreply.github.com>
2023-11-26 18:33:59 +0000

    dom basics

---
+++ 06_dom/two.js
@@ -0,0 +1,23 @@
+//  4 Pillars of DOM
+
+// 1.selection of an element
+// 2.changing html
+// 3.changing css
+// 4.event listener
+
+// var a = document.querySelector("h1");
+// console.log(a);
+
+//  b=a.innerHTML;
+//  console.log(b);
+
+// var c = document.querySelector("h1");
+// c.innerHTML="Everday";
+// or
+//var c = document.querySelector("h1").innerHTML="Web";
+
+// var d=document.querySelector("h1");
+// d.style.backgroundColor = "grey";
+// or
+//var d=document.querySelector("h1").style.backgroundColor = "black";
+
